8148646e5c21cad87b7c57d69a0a203eb891750a
Felipe <felipe.cabrera@correo.ucu.edu.uy>
2020-09-12 19:40:04 -0300

    tests 1-4

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -13,9 +13,28 @@ namespace Tests
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void Nombre_Correcto()
         {
-            // Insertá tu código  de pruebaaquí
+            Person Jorge = new Person("Jorge Rial", "5.302.011-2");
+            Assert.AreEqual("Jorge Rial" , Jorge.Name);
+        }
+        [Test]
+        public void Cedula_Correcta()
+        {
+            Person Jorge = new Person("Jorge Rial", "5.302.011-2");
+            Assert.AreEqual("5.302.011-2" , Jorge.ID);
+        }
+        [Test]
+        public void Nombre_Vacio()
+        {
+            Person Jorge = new Person("", "5.302.011-2");
+            Assert.AreEqual(null , Jorge.Name);
+        }
+        [Test]
+        public void Cedula_Invalida()
+        {
+            Person Jorge = new Person("Jorge Rial", "5.302.011-1");
+            Assert.AreEqual(null , Jorge.ID);
         }
     }
 }
